a2af75d97d4205d6e0f017dbb55482ec2285e318
Louis <louis_foy@yahoo.co.uk>
2021-01-09 23:16:50 +0000

    Monitors should only break while spinning

--- Assets/Core/Scripts/Items/Monitor.cs
+++ Assets/Core/Scripts/Items/Monitor.cs
@@ -70,7 +70,7 @@ public class Monitor : NetworkBehaviour, IMovementCollisions
 
     public void OnMovementCollidedBy(Movement source, bool isReconciliation)
     {
-        if (source is CharacterMovement character)
+        if (source is CharacterMovement character && (character.state & CharacterMovement.State.Jumped) != 0)
         {
             // bounce off
             if (character.velocity.y <= -1.0f)
