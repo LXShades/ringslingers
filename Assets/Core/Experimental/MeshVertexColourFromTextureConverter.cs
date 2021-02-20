d5f0e52d59f2651849f5977209d7a808a32afc9c
Louis <louis_foy@yahoo.co.uk>
2021-02-20 16:02:20 +0000

    Some experimental shading stuff, moving experimental files to their own folder

---
+++ Assets/Core/Experimental/MeshVertexColourFromTextureConverter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scans all objects in the scene with affectedShader as their shader and colours their vertices based on their textures
+/// </summary>
+public class MeshVertexColourFromTextureConverter : MonoBehaviour
+{
+    public Shader affectedShader;
+
+    [Range(0, 250)]
+    public float fadeStart;
+    [Range(0, 250)]
+    public float fadeEnd;
+
+    HashSet<Mesh> affectedMeshes = new HashSet<Mesh>();
+
+    private void Awake()
+    {
+        foreach (MeshFilter filter in FindObjectsOfType<MeshFilter>())
+        {
+            Mesh mesh = filter.sharedMesh;
+            MeshRenderer renderer = filter.GetComponent<MeshRenderer>();
+
+            if (mesh && renderer && !affectedMeshes.Contains(mesh))
+            {
+                Vector2[] uvs = mesh.uv;
+                Color32[] colours = new Color32[mesh.vertexCount];
+
+                for (int m = 0; m < mesh.subMeshCount; m++)
+                {
+                    if (m >= renderer.sharedMaterials.Length)
+                        break; // submeshes doesn't match material count
+
+                    Material mat = renderer?.sharedMaterials[m];
+                    Texture2D tex = mat.GetTexture("_MainTex") as Texture2D;
+
+                    if (mat && tex && mat.shader == affectedShader)
+                    {
+                        if (!tex.isReadable)
+                            continue;
+
+                        int width = tex.width, height = tex.height;
+                        var submesh = mesh.GetSubMesh(m);
+                        Color32[] pixels = tex.GetPixels32();
+
+                        if (pixels.Length == 0)
+                            continue;
+
+                        for (int i = 0; i < submesh.vertexCount; i++)
+                        {
+                            Vector2 uv = uvs[submesh.firstVertex + i];
+                            uv = new Vector2((uv.x % 1f + 1f) % 1f, (uv.y % 1f + 1f) % 1f);
+
+                            colours[submesh.firstVertex + i] = pixels[(int)((uv.y * height) % height) * width + (int)((uv.x * width) % width)];
+                        }
+
+                        mat.SetFloat("_FadeStart", fadeStart);
+                        mat.SetFloat("_FadeEnd", fadeEnd);
+                    }
+                }
+
+                mesh.SetColors(colours);
+                affectedMeshes.Add(mesh);
+            }
+        }
+    }
+}
---
+++ Assets/Core/Experimental/TestSpyroFadeout.shader
@@ -0,0 +1,65 @@
+﻿Shader "Custom/TestSpyroFadeout"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+        _FadeStart("FadeStart", Range(0,100)) = 0.0
+        _FadeEnd("FadeEnd", Range(0,100)) = 0.0
+        [ToggleOff] _SpecularHighlights("Specular Highlights", Float) = 1.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+        #pragma shader_feature_local _SPECULARHIGHLIGHTS_OFF
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float3 worldPos;
+            float4 color : COLOR;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        half _FadeStart;
+        half _FadeEnd;
+        fixed4 _Color;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            float4 screenSpace = mul(UNITY_MATRIX_VP, float4(IN.worldPos, 1));
+            screenSpace = screenSpace / screenSpace.w;
+            half depth = LinearEyeDepth(screenSpace.z);
+            fixed4 c = lerp(tex2D(_MainTex, IN.uv_MainTex), IN.color, clamp((depth - _FadeStart) / (_FadeEnd - _FadeStart), 0, 1)) * _Color;
+
+            o.Albedo = c.rgb;
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
