aba799a2dd66cdb1438a385faf35cb08765d4c6b
Louis <louis_foy@yahoo.co.uk>
2021-11-25 23:38:00 +0000

    Adding VSync selector to menus

---
+++ Assets/Core/Scripts/UI/Elements/VsyncSelector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VsyncSelector : MonoBehaviour
+{
+    public Toggle toggle;
+
+    private void OnEnable()
+    {
+        toggle.isOn = (QualitySettings.vSyncCount == 1);
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+    }
+    private void OnDisable()
+    {
+        toggle.onValueChanged.RemoveListener(OnToggleChanged);
+    }
+
+    private void Update()
+    {
+        if (toggle.isOn != (QualitySettings.vSyncCount == 1))
+            toggle.SetIsOnWithoutNotify(QualitySettings.vSyncCount == 1);
+    }
+
+    private void OnToggleChanged(bool val)
+    {
+        QualitySettings.vSyncCount = val ? 1 : 0;
+    }
+}
